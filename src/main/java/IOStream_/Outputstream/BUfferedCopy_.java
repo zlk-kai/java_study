157d776b4fb6460e08d64f21c5c23fe9c72aca43
zlk-kai <zlk990414@163.com>
2025-06-02 01:16:11 +0800

    反射：710 711 712 713 714 715 716 717 718 IO学习：610 611 612 613 614 615 616 617 618 619 620 621 622 623 624 625 626 627 628

---
+++ src/main/java/IOStream_/Outputstream/BUfferedCopy_.java
@@ -0,0 +1,26 @@
+package IOStream_.Outputstream;
+
+import java.io.*;
+
+@SuppressWarnings("all")
+public class BUfferedCopy_ {
+    public static void main(String[] args)throws IOException {
+        String inputfile = "Z:\\SONE-762-C.mp4";
+        String outputfile = "D:\\Develop\\IdeaProjects\\java_study\\src\\main\\java\\IOStream_\\Outputstream\\1.mp4";
+        bufferedCopy01(inputfile, outputfile);
+    }
+    public static void bufferedCopy01(String inputfile,String outputfile) throws IOException {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        bis = new BufferedInputStream(new FileInputStream(inputfile));
+        bos = new BufferedOutputStream(new FileOutputStream(outputfile));
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = bis.read(buf)) != -1) {
+            bos.write(buf, 0, len);
+            System.out.println(len);
+        }
+        bis.close();
+        bos.close();
+    }
+}
