d7c2721f1ce9e28a1c016a14f74c9baaa4a889c8
zlk-kai <zlk990414@163.com>
2025-06-06 14:16:46 +0800

    反射：710 711 712 713 714 715 716 717 718 IO学习：610 611 612 613 614 615 616 617 618 619 620 621 622 623 624 625 626 627 628 629 630 631 632 633 634 635 636 637 638 639 640 枚举注解：425 426 427 428 429 430 431 432 433 434 435 异常：

---
+++ src/main/java/Exception_/Exception_.java
@@ -0,0 +1,16 @@
+package Exception_;
+
+/**
+ * @author 张林凯
+ * @version 1.0
+ * 异常处理
+ */
+public class Exception_ {
+    public static void main(String[] args) throws Exception {
+        int num1 = 10;
+        int num2 = 0;
+        //抛出异常后程序自动退出，不再执行
+        int res = num1 / num2;
+        System.out.println(res);
+    }
+}
